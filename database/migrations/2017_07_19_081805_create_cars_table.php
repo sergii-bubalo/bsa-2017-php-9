f44964c99e130bafbd8f7bbe541c1c817c0d4f3c
Sergii Bubalo <sergii.bubalo@gmail.com>
2017-07-19 13:12:52 +0300

    Migrations added

---
+++ database/migrations/2017_07_19_081805_create_cars_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('model');
+            $table->string('color');
+            $table->string('registration_number');
+            $table->integer('year')->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
